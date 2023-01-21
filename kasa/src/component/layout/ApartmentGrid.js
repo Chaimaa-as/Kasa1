a5a611e83bc388fe04d7e8eee13004e9417b48ba
chaimaa asri <chaimaa.asri@gmail.com>
2023-01-21 14:16:20 +0100

    suite squelette widget DisclosePanel et ImageSlider

--- kasa/src/component/layout/ApartmentGrid.js
+++ kasa/src/component/layout/ApartmentGrid.js
@@ -1,50 +1,61 @@
-
 import React, { useEffect,useState } from 'react';
-
 import Apartment from '../layout/Apartment';
+import ApartmentsList from "../../logement.json";
 
 
 const ApartmentGrid = () => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  
+  //SI ON VEUT RECUPERER LES DATA A TRAVERS UNE API : 
+//   const [error, setError] = useState(null);
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   const [items, setItems] = useState([]);
 
-  // Remarque : le tableau vide de dépendances [] indique
-  // que useEffect ne s’exécutera qu’une fois, un peu comme
-  // componentDidMount()
-  useEffect(() => {
-    fetch("./logement.json")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Remarque : il faut gérer les erreurs ici plutôt que dans
-        // un bloc catch() afin que nous n’avalions pas les exceptions
-        // dues à de véritables bugs dans les composants.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+//   // Remarque : le tableau vide de dépendances [] indique
+//   // que useEffect ne s’exécutera qu’une fois,
+//   useEffect(() => {
+//     fetch("./logement.json")
+//       .then(res => res.json())
+//       .then(
+//         (result) => {
+//           setIsLoaded(true);
+//           setItems(result);
+//         },
+//         // Remarque : il faut gérer les erreurs ici plutôt que dans
+//         // un bloc catch() afin que nous n’avalions pas les exceptions
+//         // dues à de véritables bugs dans les composants.
+//         (error) => {
+//           setIsLoaded(true);
+//           setError(error);
+//         }
+//       )
+//   }, [])
 
-  if (error) {
-    return <div>Erreur : {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Chargement...</div>;
-  } else {
-    return (
-      <div className='apartementGrid'>
+//   if (error) {
+//     return <div>Erreur : {error.message}</div>;g
+//   } else if (!isLoaded) {
+//     return <div>Chargement...</div>;
+//   } else {
+//     return (
+//       <div className='apartementGrid'>
 
-
-         {items.map(item => (
-          <Apartment key={item.id} item={item}/>
+//          {items.map(item => (
+//           <Apartment key={item.id} item={item}/>
             
-        ))}
-      </div>
-    );
-  }
+//         ))}
+//       </div>
+//     );
+//   }
+
+//ON RECUPERE LES DATA DIRECTEMENT EN IMPORTANT UN JSON
+return (
+          <div className='apartementGrid'>
+    
+             {ApartmentsList.map(item => (
+              <Apartment key={item.id} item={item}/>
+                
+            ))}
+          </div>
+        );
+
 }
 export default ApartmentGrid;
