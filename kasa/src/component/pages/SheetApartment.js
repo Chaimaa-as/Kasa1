291bef363c9f2535f2be366070ecfe3af9a14814
chaimaa asri <chaimaa.asri@gmail.com>
2023-02-14 19:12:20 +0100

    apart-responsive

--- kasa/src/component/pages/SheetApartment.js
+++ kasa/src/component/pages/SheetApartment.js
@@ -10,7 +10,6 @@ import ImagesSlider from '../widgets/ImagesSlider';
 import Tags from '../widgets/Tags';
 import Ratings from '../widgets/Ratings';
 import Host from '../widgets/Host';
-// import ApartHeader from '../layout/ApartHeader';
 
 
 const SheetApartment = () => {
@@ -22,37 +21,39 @@ const SheetApartment = () => {
             <Header/>
             <ImagesSlider urls={apartment.pictures} />
 
-            <section className='apartment'>
+            <section className='apartmentDetails'>
 
-                <div className='flex flex-apart'>
-                    <div className='apartment__title-location'>
-                        <h1 className='apartment__title'>{apartment.title}</h1>
-                        <p className='apartment__location'>{apartment.location}</p>
+                <div className='apartment-abstracts'>
+
+                    <div className='apartment__header'>
+                        <div className='apartment__title-location'>
+                            <h1 className='apartment__title'>{apartment.title}</h1>
+                            <p className='apartment__location'>{apartment.location}</p>
+                        </div>
+                        <div className='apartment__tags'>
+                            <Tags tagsList = {apartment.tags}/>
+                        </div>
                     </div>
-                    <div className='apartment__host'>
-                        <Host hostData={apartment.host} />
+
+                    <div className='apartment__owner'>
+                        <div className='apartment__host'>
+                            <Host hostData={apartment.host} />
+                        </div>
+                        <div className='apartment__ratings'>
+                            <Ratings rating={apartment.rating} />
+                        </div>
                     </div>
                 </div>
-
-
-                <div className='flex flex-host'>
-                    <div className='apartment__tags'>
-                        <Tags tagsList = {apartment.tags}/>
+            
+                <div className='apartment-content'>
+                    <div className='apartment-content__details'>
+                        <DisclosePanel title="Desciption" text={apartment.description} />
                     </div>
-                    <div className='apartment__ratings'>
-                        <Ratings rating={apartment.rating} />
+                    <div className='apartment-content__details'>
+                        <DisclosePanel title="Equipement" text={apartment.equipments} />
                     </div>
                 </div>
-            </section>       
-
-
-            <section className='apartment-content'>
-                <div className='apartment-content__details'>
-                    <DisclosePanel title="Desciption" text={apartment.description} />
-                </div>
-                <div className='apartment-content__details'>
-                    <DisclosePanel title="Equipement" text={apartment.equipments} />
-                </div>
+         
             </section>
 
             <Footer/>
