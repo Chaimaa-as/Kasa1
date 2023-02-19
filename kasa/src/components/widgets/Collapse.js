706e11f43f3db62f91715422f3545e576e422970
chaimaa asri <chaimaa.asri@gmail.com>
2023-02-19 13:40:08 +0100

    refactor Collapse

--- kasa/src/components/widgets/Collapse.js
+++ kasa/src/components/widgets/Collapse.js
@@ -1,44 +1,34 @@
 import React ,{ useState }from 'react';
 
 const Collapse = (props) => {
-    console.log("props in collapse", props) 
+
     const [isVisible,setIsVisible]=useState(false)
-
-    function toggle(){
-        setIsVisible(!isVisible)
-    }
-
-    function drawPanel(){
-        // on regarde si l'attribut text passé en parametre du composant est un tableau.
-        if (props.text instanceof Array){
-            // si oui on affiche chaque element du tableau dans un li 
-            return (
-                <ul className='collapsePanel__equipment' >
-                    {  
-                        props.text.map((text,index)=>(
-                            <li key={index}>{text}</li>
-                            ))                        
-                        }
-                    </ul>
-                    )
-                }
-        // sinon on affiche directement le texte dans une div
-        else{
-            return (<div className='collapsePanel__description'>{props.text}</div>);
-        }              
-    }
+    
 
     return (
         <div className='collapsePanel'>
-            <div className='collapsePanel__title'>
-                {props.title} 
-                <button className={isVisible?'collapsePanel__arrow--up active':'collapsePanel__arrow--down active'} onClick={toggle}></button>
+            <div className='collapsePanel__title'>{props.title} 
+                <button 
+                    className={isVisible?'collapsePanel__arrow--up active':'collapsePanel__arrow--down active'} 
+                    onClick={()=>setIsVisible(!isVisible)}></button>
             </div>
           
             {/* // si le state isVisible est vrai : appel de la fonction 
             // sinon on affiche juste des guillemets */}
             {
-                isVisible?drawPanel():""
+                isVisible?
+                    props.text instanceof Array?
+                    <ul className='collapsePanel__equipment' >
+                        {  
+                            props.text.map((text,index)=>(
+                                <li key={index}>{text}</li>
+                                ))                        
+                            }
+                        </ul>
+                    :
+                    <div className='collapsePanel__description'>{props.text}</div>
+                :
+                <></>
             }
         </div>
     );
