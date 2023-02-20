828af84592f7f7aa4195aae48b05d152f2de9d32
chaimaa asri <chaimaa.asri@gmail.com>
2023-02-20 10:54:43 +0100

    supression import inutiles Banner

--- kasa/src/components/layout/Banner.js
+++ kasa/src/components/layout/Banner.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 const Banner = ({showTxt}) => {
 
-	
- 
 	return (
 		<section className={showTxt ? 'banner banner__about' : 'banner banner__hero'}>
 			{!showTxt && <h1>Chez vous, <br/>partout et ailleurs</h1>}
